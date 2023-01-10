01ec8de73d0a00e6134f853bee137f84eafd8ddf
d-willb <39524812+d-willb@users.noreply.github.com>
2023-01-10 09:26:09 +0100

    Update popup.js

--- js/agence-o/popup.js
+++ js/agence-o/popup.js
@@ -4,34 +4,34 @@
  * @created 08/02/2017
  */
 
-;(function ($) {
-
-  var $popup = $('.popup'),
-    $popup_container = $popup.find('.popup__container'),
-    $btn_close = $popup.find('.popup__close');
-
-  if ($.cookie("popup") === '1') {
-    $popup.hide();
-  } else {
-    $popup.removeClass('visually-hidden');
-    $popup.show();
-  }
-
-  $btn_close.on('click', function (e) {
-    e.preventDefault();
-    $popup.animate({'opacity': 0}, 300, function () {
-      $popup.remove();
-      $.cookie("popup", 1, {expires: 1});
-    });
-  });
-
-  $popup.on('click', function (e) {
-    e.preventDefault();
-    $btn_close.click();
-  });
-
-  $popup_container.on('click', function (e) {
-    e.stopPropagation();
-  });
-
-})(jQuery);
+//;(function ($) {
+//
+//  var $popup = $('.popup'),
+//    $popup_container = $popup.find('.popup__container'),
+//    $btn_close = $popup.find('.popup__close');
+//
+//  if ($.cookie("popup") === '1') {
+//    $popup.hide();
+//  } else {
+//    $popup.removeClass('visually-hidden');
+//    $popup.show();
+//  }
+//
+//  $btn_close.on('click', function (e) {
+//    e.preventDefault();
+//    $popup.animate({'opacity': 0}, 300, function () {
+//      $popup.remove();
+//      $.cookie("popup", 1, {expires: 1});
+//    });
+//  });
+//
+//  $popup.on('click', function (e) {
+//    e.preventDefault();
+//    $btn_close.click();
+//  });
+//
+//  $popup_container.on('click', function (e) {
+//    e.stopPropagation();
+//  });
+//
+//})(jQuery);
